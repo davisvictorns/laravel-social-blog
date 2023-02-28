392f1335d64736fccd82b0af8e08f7110e0639e3
Davis Victor Nascimento da Silva <davis_victor@hotmail.com>
2023-02-28 20:43:46 -0300

    User authentication

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,8 +17,38 @@ class UserController extends Controller
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
 
-        User::create($incomingFields);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+        return redirect('/')->with('success', 'Thank you for creating an account.');
+    }
 
-        return 'this is the register function on usercontroller.';
+    public function login(Request $request) {
+        $incomingFields = $request->validate([
+            'loginusername' => 'required',
+            'loginpassword' => 'required'
+        ]);
+
+        if(auth()->attempt([
+            'username' => $incomingFields["loginusername"],
+            'password' => $incomingFields["loginpassword"]
+        ])) {
+            $request->session()->regenerate();
+            return redirect('/')->with('success', 'You are now logged in.');
+        } else {
+            return redirect('/')->with('failure', 'Invalid login.');
+        }
+    }
+
+    public function logout() {
+        auth()->logout();
+        return redirect('/')->with('success', 'You are now logged out.');
+    }
+
+    public function showCorrectHomepage() {
+        if(auth()->check()){
+            return view('homepage-feed');
+        } else {
+            return view('homepage');
+        }
     }
 }

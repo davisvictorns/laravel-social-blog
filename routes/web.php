002c389dcc3b8ddda7fc38058d0bd0b5c6720ceb
Davis Victor Nascimento da Silva <davis_victor@hotmail.com>
2023-03-03 00:49:25 -0300

    Post creation

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -14,8 +15,14 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [UserController::class, "showCorrectHomepage"]);
 
+// User
+Route::get('/', [UserController::class, "showCorrectHomepage"]);
 Route::post('/register', [UserController::class, "register"]);
 Route::post('/login', [UserController::class, "login"]);
 Route::post('/logout', [UserController::class, "logout"]);
+
+
+// Blog Post
+Route::get('/create-post', [PostController::class, "showCreateForm"]);
+Route::post('/create-post', [PostController::class, "storeNewPost"]);

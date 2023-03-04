186d3b10c94764333c3984af2a8078e8b372c982
Davis Victor Nascimento da Silva <davis_victor@hotmail.com>
2023-03-04 17:05:05 -0300

    Using policies

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,8 +27,31 @@ class PostController extends Controller
         return redirect("/post/{$newPost->id}")->with('success', 'New post created successfully!');
     }
 
-    public function showCreatedPost(Post $post){
+    public function showCreatedPost(Post $post) {
         $post['body'] = strip_tags(Str::markdown($post->body), '<ul><li><strong><h1><h2><h3><br><p><em>');
         return view('single-post', ['post' => $post]);
+    }
+
+    public function delete(Post $post) {
+        $post->delete();
+        return redirect('/profile/' . auth()->user()->username);
+    }
+
+    public function showEditForm(Post $post) {
+        return view('edit-post', ['post' => $post]);
+    }
+
+    public function actuallyUpdate(Post $post, Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        $post->update($incomingFields);
+
+        return back()->with('success', 'Post edited successfully!');
     }
 }

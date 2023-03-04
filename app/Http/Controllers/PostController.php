4fe6e96e085fbb0406c9ee1b17531184bbd5c748
Davis Victor Nascimento da Silva <davis_victor@hotmail.com>
2023-03-03 22:31:37 -0300

    Post exibition

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -21,8 +22,13 @@ class PostController extends Controller
         $incomingFields['body'] = strip_tags($incomingFields['body']);
         $incomingFields['user_id'] = auth()->id();
 
-        Post::create($incomingFields);
+        $newPost = Post::create($incomingFields);
 
-        return "haey";
+        return redirect("/post/{$newPost->id}")->with('success', 'New post created successfully!');
+    }
+
+    public function showCreatedPost(Post $post){
+        $post['body'] = strip_tags(Str::markdown($post->body), '<ul><li><strong><h1><h2><h3><br><p><em>');
+        return view('single-post', ['post' => $post]);
     }
 }

3a2dc94399c8b5f1bd4d8bbfb1da787a0986dfd9
Davis Victor Nascimento da Silva <davis_victor@hotmail.com>
2023-03-05 18:05:21 -0300

    Dinamic page title

--- resources/views/profile-posts.blade.php
+++ resources/views/profile-posts.blade.php
@@ -1,4 +1,4 @@
-<x-profile :sharedData="$sharedData">
+<x-profile :sharedData="$sharedData" doctitle="{{ auth()->user()->username }}'s Profile">
   <div class="list-group">
     @foreach ($posts as $post)
       <a href="/post/{{ $post->id }}" class="list-group-item list-group-item-action">

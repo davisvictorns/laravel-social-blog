4bbf696e39e435c2be432bde4877e07f13f00d93
Davis Victor Nascimento da Silva <davis_victor@hotmail.com>
2023-03-06 23:52:05 -0300

    Search frontend implementation with npm mix

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,7 +16,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="/main.css" />
+    <link rel="stylesheet" href="/css/app.css" />
   </head>
   <body>
     <header class="header-bar mb-3">
@@ -82,6 +82,7 @@
       <script>
         $('[data-toggle="tooltip"]').tooltip()
       </script>
+      <script src="/js/app.js"></script>
     </body>
 </html>
   
